3514b2927a49bf53955aa98b61fcce8dbed6119f
Eli-Debusk <elidebusk.edu@gmail.com>
2023-12-03 18:39:51 -0800

    added Phoenix Library and Falcon500 class

---
+++ src/main/java/frc/robot/rosemont/util/Falcon500.java
@@ -0,0 +1,14 @@
+package frc.robot.rosemont.util;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+public class Falcon500 extends TalonFX{
+    public Falcon500(int deviceNumber) {
+        super(deviceNumber);
+    }
+
+    public void setPower(double power) {
+        this.set(ControlMode.PercentOutput, power);
+    }
+}
