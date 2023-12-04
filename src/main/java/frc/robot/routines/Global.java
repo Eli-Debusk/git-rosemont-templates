441f100e6a07e3b6431988eebd8f3cc99e551149
Eli-Debusk <elidebusk.edu@gmail.com>
2023-12-03 17:27:37 -0800

    a commit

---
+++ src/main/java/frc/robot/routines/Global.java
@@ -0,0 +1,16 @@
+package frc.robot.routines;
+
+import edu.wpi.first.wpilibj.XboxController;
+
+public class Global {
+
+    public XboxController controller = new XboxController(0);
+    
+    public static void initialize() {
+
+    }
+
+    public static void periodic() {
+
+    }
+}
