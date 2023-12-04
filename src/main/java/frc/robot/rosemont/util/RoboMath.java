c60e4ae4b1522895fed45a038bdd9b5e6e504906
Eli-Debusk <elidebusk.edu@gmail.com>
2023-12-03 20:10:06 -0800

    0

--- src/main/java/frc/robot/rosemont/util/RoboMath.java
+++ src/main/java/frc/robot/rosemont/util/RoboMath.java
@@ -1,6 +1,8 @@
 package frc.robot.rosemont.util;
 
 public class RoboMath {
+
+    //Replicates the Math.clip() method from Python, useful for limiting a value between a min and max range
     public static double clip(double value, double min, double max) {
         return Math.max(min, Math.min(value, max));
     }
