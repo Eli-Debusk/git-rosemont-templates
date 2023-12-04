881415f0fa223ab63096f30df94bb958a05d2c92
Eli-Debusk <elidebusk.edu@gmail.com>
2023-12-03 18:42:09 -0800

    added comments to Falcon500

--- src/main/java/frc/robot/rosemont/util/Falcon500.java
+++ src/main/java/frc/robot/rosemont/util/Falcon500.java
@@ -4,11 +4,14 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 public class Falcon500 extends TalonFX{
+    
+    //Builtin method for initializing TalonFX
     public Falcon500(int deviceNumber) {
         super(deviceNumber);
     }
 
+    //Create and ease of use setPower method to save space
     public void setPower(double power) {
-        this.set(ControlMode.PercentOutput, power);
+        this.set(ControlMode.PercentOutput, power); //Sets power of the motor to given power value
     }
 }
