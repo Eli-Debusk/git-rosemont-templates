6dc42a564d9fcd96ff3768f0c4e0c7063483a7a0
Eli-Debusk <elidebusk.edu@gmail.com>
2023-12-03 17:26:46 -0800

    adding basic timed methods for organization

---
+++ src/main/java/frc/robot/routines/Autonomous.java
@@ -0,0 +1,11 @@
+package frc.robot.routines;
+
+public class Autonomous {
+    public static void initialize() {
+
+    }
+    
+    public static void periodic() {
+
+    }
+}
---
+++ src/main/java/frc/robot/routines/TeleOp.java
@@ -0,0 +1,11 @@
+package frc.robot.routines;
+
+public class TeleOp {
+    public static void initialize() {
+
+    }
+
+    public static void periodic() {
+
+    }
+}
